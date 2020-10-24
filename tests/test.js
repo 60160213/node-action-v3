65ee220826a9868c83e5cc101a2c2c40bc9c7041
sasiwimol chuchum <61160269@go.buu.ac.th>
2020-10-24 15:27:33 +0700

    add test GET '/'

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,16 @@
 const assert = require('assert')
-
+const { request } = require('http')
+const require = require('supertest')
+const assert = require('../index')
 describe('Validate setting the test', () => {
     it('should be ok', () => {
-        assert.ok(false)
+        assert.ok(true)
     })
+})
+
+describe('GET "/"',()=> {
+    it('respond with Hello!', (done) => {
+        request(app).get('/').expect('Hello!',done)
+    })
+
 })
